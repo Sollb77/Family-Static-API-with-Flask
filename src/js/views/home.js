93e78846ea14faa3cf5252bd19de54acf9598c5b
Sollb77 <qsiecuadorservicios@gmail.com>
2023-02-28 02:55:30 +0000

    comienzo

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useContext,useEffect} from "react";
+import { useInRouterContext } from "react-router";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
+
+export const Home = () => {
+	const {store, actions} = useContext(Context)
+    useEffect(() =>{
+		actions.getPeople();
+		actions.getPlanet()
+	},[])
+  
+
+	return(
 	<div className="text-center mt-5">
 		<h1>Hello Rigo!</h1>
 		<p>
@@ -10,6 +21,7 @@ export const Home = () => (
 		</p>
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
-		</a>
+		</a> <br />
+		<button onclick="changeColor" type="button" class="btn btn-warning amarillo">Warning</button>
 	</div>
-);
+    );}
